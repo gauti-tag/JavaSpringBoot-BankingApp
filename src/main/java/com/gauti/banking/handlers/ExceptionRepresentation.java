6b5a007bcf6a4a7a36bfe3fd6b17d4c06c7022e2
GAUTIER TIEHOULE <gautier.tiehoule@ngser.com>
2026-01-16 21:44:31 +0000

    implémentation le gestionnaire des exceptions globale

---
+++ src/main/java/com/gauti/banking/handlers/ExceptionRepresentation.java
@@ -0,0 +1,20 @@
+
+package com.gauti.banking.handlers;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY) // ne va pas retourner une valeur null si une donnée n'existe pas
+class ExceptionRepresentation {
+
+    private String errorMessage; 
+    private String errorSource; 
+    private Set<String> validationErrors; 
+
+}
---
+++ src/main/java/com/gauti/banking/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.gauti.banking.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.gauti.banking.exceptions.ObjectValidationException;
+import com.gauti.banking.exceptions.OperationNonPermittedException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ObjectValidationException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(ObjectValidationException exception){
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                                                    .errorMessage("Object not valid exception has occured")
+                                                    .errorSource(exception.getViolationSource())
+                                                    .validationErrors(exception.getViolations())
+                                                    .build();
+        
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(representation);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(EntityNotFoundException exception){
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                                                    .errorMessage(exception.getMessage())
+                                                    .build();
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(representation);
+    }
+
+    
+    @ExceptionHandler(OperationNonPermittedException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(OperationNonPermittedException exception){
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                                                    .errorMessage(exception.getMessage())
+                                                    .build();
+        return ResponseEntity
+            .status(HttpStatus.NOT_ACCEPTABLE)
+            .body(representation);
+    }
+
+}
